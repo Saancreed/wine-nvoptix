8ce1df5024e3468aac99f0ef4200d49d3717a44b
Krzysztof Bogacki <krzysztof.bogacki@leancode.pl>
2022-03-05 03:02:22 +0100

    Implement support for ABI 22 / Optix 7.0.0

--- src/nvoptix.c
+++ src/nvoptix.c
@@ -45,11 +45,13 @@ static OptixResult (*poptixQueryFunctionTable)(int abiId, unsigned int numOption
 #include "nvoptix_47.h"
 #include "nvoptix_41.h"
 #include "nvoptix_36.h"
+#include "nvoptix_22.h"
 
 static OptixFunctionTable_55 optixFunctionTable_55;
 static OptixFunctionTable_47 optixFunctionTable_47;
 static OptixFunctionTable_41 optixFunctionTable_41;
 static OptixFunctionTable_36 optixFunctionTable_36;
+static OptixFunctionTable_22 optixFunctionTable_22;
 
 /* OptiX ABI = 55 / SDK 7.4.0 */
 
@@ -1131,6 +1133,258 @@ static OptixResult __cdecl optixDenoiserComputeIntensity_36(OptixDenoiser handle
     return optixFunctionTable_36.optixDenoiserComputeIntensity(handle, stream, inputImage, outputIntensity, scratch, scratchSizeInBytes);
 }
 
+/* OptiX ABI = 22 / SDK 7.0.0 */
+
+static const char *__cdecl optixGetErrorName_22(OptixResult result)
+{
+    TRACE("(%d)\n", result);
+    return optixFunctionTable_22.optixGetErrorName(result);
+}
+
+static const char *__cdecl optixGetErrorString_22(OptixResult result)
+{
+    TRACE("(%d)\n", result);
+    return optixFunctionTable_22.optixGetErrorString(result);
+}
+
+static OptixResult __cdecl optixDeviceContextCreate_22(CUcontext fromContext, const OptixDeviceContextOptions_22 *options, OptixDeviceContext *context)
+{
+    TRACE("(%p, %p, %p)\n", fromContext, options, context);
+
+    OptixDeviceContextOptions_22 opts = *options;
+
+    if (opts.logCallbackFunction || opts.logCallbackLevel)
+    {
+        FIXME("log callback not supported\n");
+        opts.logCallbackFunction = NULL;
+        opts.logCallbackLevel = 0;
+    }
+
+    return optixFunctionTable_22.optixDeviceContextCreate(fromContext, &opts, context);
+}
+
+static OptixResult __cdecl optixDeviceContextDestroy_22(OptixDeviceContext context)
+{
+    TRACE("(%p)\n", context);
+    return optixFunctionTable_22.optixDeviceContextDestroy(context);
+}
+
+static OptixResult __cdecl optixDeviceContextGetProperty_22(OptixDeviceContext context, int property, void *value, size_t sizeInBytes)
+{
+    TRACE("(%p, %d, %p, %zu)\n", context, property, value, sizeInBytes);
+    return optixFunctionTable_22.optixDeviceContextGetProperty(context, property, value, sizeInBytes);
+}
+
+static OptixResult __cdecl optixDeviceContextSetLogCallback_22(OptixDeviceContext context, OptixLogCallback_22 callbackFunction, void *callbackData, unsigned int callbackLevel)
+{
+    FIXME("(%p, %p, %p, %u): stub\n", context, callbackFunction, callbackData, callbackLevel);
+    return OPTIX_SUCCESS;
+}
+
+static OptixResult __cdecl optixDeviceContextSetCacheEnabled_22(OptixDeviceContext context, int enabled)
+{
+    TRACE("(%p, %d)\n", context, enabled);
+    return optixFunctionTable_22.optixDeviceContextSetCacheEnabled(context, enabled);
+}
+
+static OptixResult __cdecl optixDeviceContextSetCacheLocation_22(OptixDeviceContext context, const char *location)
+{
+    TRACE("(%p, %s)\n", context, location);
+
+    if (!location) return OPTIX_ERROR_DISK_CACHE_INVALID_PATH;
+
+    WCHAR location_wide[MAX_PATH];
+
+    MultiByteToWideChar(CP_ACP, 0, location, -1, location_wide, ARRAY_SIZE(location_wide));
+
+    char *unix_location = wine_get_unix_file_name(location_wide);
+
+    OptixResult result = optixFunctionTable_22.optixDeviceContextSetCacheLocation(context, unix_location);
+
+    HeapFree(GetProcessHeap(), 0, unix_location);
+
+    return result;
+}
+
+static OptixResult __cdecl optixDeviceContextSetCacheDatabaseSizes_22(OptixDeviceContext context, size_t lowWaterMark, size_t highWaterMark)
+{
+    TRACE("(%p, %zu, %zu)\n", context, lowWaterMark, highWaterMark);
+    return optixFunctionTable_22.optixDeviceContextSetCacheDatabaseSizes(context, lowWaterMark, highWaterMark);
+}
+
+static OptixResult __cdecl optixDeviceContextGetCacheEnabled_22(OptixDeviceContext context, int *enabled)
+{
+    TRACE("(%p, %p)\n", context, enabled);
+    return optixFunctionTable_22.optixDeviceContextGetCacheEnabled(context, enabled);
+}
+
+static OptixResult __cdecl optixDeviceContextGetCacheLocation_22(OptixDeviceContext context, char *location, size_t locationSize)
+{
+    TRACE("(%p, %p, %zu)\n", context, location, locationSize);
+
+    OptixResult result = optixFunctionTable_22.optixDeviceContextGetCacheLocation(context, location, locationSize);
+
+    if (result != OPTIX_SUCCESS) return result;
+
+    WCHAR *dos_location = wine_get_dos_file_name(location);
+
+    if (!WideCharToMultiByte(CP_ACP, 0, dos_location, -1, location, locationSize, NULL, NULL)) result = OPTIX_ERROR_INVALID_VALUE;
+
+    HeapFree(GetProcessHeap(), 0, dos_location);
+
+    return result;
+}
+
+static OptixResult __cdecl optixDeviceContextGetCacheDatabaseSizes_22(OptixDeviceContext context, size_t *lowWaterMark, size_t *highWaterMark)
+{
+    TRACE("(%p, %p, %p)\n", context, lowWaterMark, highWaterMark);
+    return optixFunctionTable_22.optixDeviceContextGetCacheDatabaseSizes(context, lowWaterMark, highWaterMark);
+}
+
+static OptixResult __cdecl optixModuleCreateFromPTX_22(OptixDeviceContext context, const void *moduleCompileOptions, const void *pipelineCompileOptions, const char *PTX, size_t PTXsize, char *logString, size_t *logStringSize, OptixModule *module)
+{
+    TRACE("(%p, %p, %p, %p, %zu, %p, %p, %p)\n", context, moduleCompileOptions, pipelineCompileOptions, PTX, PTXsize, logString, logStringSize, module);
+    return optixFunctionTable_22.optixModuleCreateFromPTX(context, moduleCompileOptions, pipelineCompileOptions, PTX, PTXsize, logString, logStringSize, module);
+}
+
+static OptixResult __cdecl optixModuleDestroy_22(OptixModule module)
+{
+    TRACE("(%p)\n", module);
+    return optixFunctionTable_22.optixModuleDestroy(module);
+}
+
+static OptixResult __cdecl optixProgramGroupCreate_22(OptixDeviceContext context, const void *programDescriptions, unsigned int numProgramGroups, const void *options, char *logString, size_t *logStringSize, OptixProgramGroup *programGroups)
+{
+    TRACE("(%p, %p, %u, %p, %p, %p, %p)\n", context, programDescriptions, numProgramGroups, options, logString, logStringSize, programGroups);
+    return optixFunctionTable_22.optixProgramGroupCreate(context, programDescriptions, numProgramGroups, options, logString, logStringSize, programGroups);
+}
+
+static OptixResult __cdecl optixProgramGroupDestroy_22(OptixProgramGroup programGroup)
+{
+    TRACE("(%p)\n", programGroup);
+    return optixFunctionTable_22.optixProgramGroupDestroy(programGroup);
+}
+
+static OptixResult __cdecl optixProgramGroupGetStackSize_22(OptixProgramGroup programGroup, void *stackSizes)
+{
+    TRACE("(%p, %p)\n", programGroup, stackSizes);
+    return optixFunctionTable_22.optixProgramGroupGetStackSize(programGroup, stackSizes);
+}
+
+static OptixResult __cdecl optixPipelineCreate_22(OptixDeviceContext context, const void *pipelineCompileOptions, const void *pipelineLinkOptions, const OptixProgramGroup *programGroups, unsigned int numProgramGroups, char *logString, size_t *logStringSize, OptixPipeline *pipeline)
+{
+    TRACE("(%p, %p, %p, %p, %u, %p, %p, %p)\n", context, pipelineCompileOptions, pipelineLinkOptions, programGroups, numProgramGroups, logString, logStringSize, pipeline);
+    return optixFunctionTable_22.optixPipelineCreate(context, pipelineCompileOptions, pipelineLinkOptions, programGroups, numProgramGroups, logString, logStringSize, pipeline);
+}
+
+static OptixResult __cdecl optixPipelineDestroy_22(OptixPipeline pipeline)
+{
+    TRACE("(%p)\n", pipeline);
+    return optixFunctionTable_22.optixPipelineDestroy(pipeline);
+}
+
+static OptixResult __cdecl optixPipelineSetStackSize_22(OptixPipeline pipeline, unsigned int directCallableStackSizeFromTraversal, unsigned int directCallableStackSizeFromState, unsigned int continuationStackSize, unsigned int maxTraversableGraphDepth)
+{
+    TRACE("(%p, %u, %u, %u, %u)\n", pipeline, directCallableStackSizeFromTraversal, directCallableStackSizeFromState, continuationStackSize, maxTraversableGraphDepth);
+    return optixFunctionTable_22.optixPipelineSetStackSize(pipeline, directCallableStackSizeFromTraversal, directCallableStackSizeFromState, continuationStackSize, maxTraversableGraphDepth);
+}
+
+static OptixResult __cdecl optixAccelComputeMemoryUsage_22(OptixDeviceContext context, const void *accelOptions, const void *buildInputs, unsigned int numBuildInputs, void *bufferSizes)
+{
+    TRACE("(%p, %p, %p, %u, %p)\n", context, accelOptions, buildInputs, numBuildInputs, bufferSizes);
+    return optixFunctionTable_22.optixAccelComputeMemoryUsage(context, accelOptions, buildInputs, numBuildInputs, bufferSizes);
+}
+
+static OptixResult __cdecl optixAccelBuild_22(OptixDeviceContext context, CUstream stream, const void *accelOptions, const void *buildInputs, unsigned int numBuildInputs, CUdeviceptr tempBuffer, size_t tempBufferSizeInBytes, CUdeviceptr outputBuffer, size_t outputBufferSizeInBytes, OptixTraversableHandle *outputHandle, const void *emittedProperties, unsigned int numEmittedProperties)
+{
+    TRACE("(%p, %p, %p, %p, %u, %p, %zu, %p, %zu, %p, %p, %u)\n", context, stream, accelOptions, buildInputs, numBuildInputs, tempBuffer, tempBufferSizeInBytes, outputBuffer, outputBufferSizeInBytes, outputHandle, emittedProperties, numEmittedProperties);
+    return optixFunctionTable_22.optixAccelBuild(context, stream, accelOptions, buildInputs, numBuildInputs, tempBuffer, tempBufferSizeInBytes, outputBuffer, outputBufferSizeInBytes, outputHandle, emittedProperties, numEmittedProperties);
+}
+
+static OptixResult __cdecl optixAccelGetRelocationInfo_22(OptixDeviceContext context, OptixTraversableHandle handle, void *info)
+{
+    TRACE("(%p, %llu, %p)\n", context, handle, info);
+    return optixFunctionTable_22.optixAccelGetRelocationInfo(context, handle, info);
+}
+
+static OptixResult __cdecl optixAccelCheckRelocationCompatibility_22(OptixDeviceContext context, const void *info, int *compatible)
+{
+    TRACE("(%p, %p, %p)\n", context, info, compatible);
+    return optixFunctionTable_22.optixAccelCheckRelocationCompatibility(context, info, compatible);
+}
+
+static OptixResult __cdecl optixAccelRelocate_22(OptixDeviceContext context, CUstream stream, const void *info, CUdeviceptr instanceTraversableHandles, size_t numInstanceTraversableHandles, CUdeviceptr targetAccel, size_t targetAccelSizeInBytes, OptixTraversableHandle *targetHandle)
+{
+    TRACE("(%p, %p, %p, %p, %zu, %p, %zu, %p)\n", context, stream, info, instanceTraversableHandles, numInstanceTraversableHandles, targetAccel, targetAccelSizeInBytes, targetHandle);
+    return optixFunctionTable_22.optixAccelRelocate(context, stream, info, instanceTraversableHandles, numInstanceTraversableHandles, targetAccel, targetAccelSizeInBytes, targetHandle);
+}
+
+static OptixResult __cdecl optixAccelCompact_22(OptixDeviceContext context, CUstream stream, OptixTraversableHandle inputHandle, CUdeviceptr outputBuffer, size_t outputBufferSizeInBytes, OptixTraversableHandle *outputHandle)
+{
+    TRACE("(%p, %p, %llu, %p, %zu, %p)\n", context, stream, inputHandle, outputBuffer, outputBufferSizeInBytes, outputHandle);
+    return optixFunctionTable_22.optixAccelCompact(context, stream, inputHandle, outputBuffer, outputBufferSizeInBytes, outputHandle);
+}
+
+static OptixResult __cdecl optixConvertPointerToTraversableHandle_22(OptixDeviceContext onDevice, CUdeviceptr pointer, int traversableType, OptixTraversableHandle *traversableHandle)
+{
+    TRACE("(%p, %p, %d, %p)\n", onDevice, pointer, traversableType, traversableHandle);
+    return optixFunctionTable_22.optixConvertPointerToTraversableHandle(onDevice, pointer, traversableType, traversableHandle);
+}
+
+static OptixResult __cdecl optixSbtRecordPackHeader_22(OptixProgramGroup programGroup, void *sbtRecordHeaderHostPointer)
+{
+    TRACE("(%p, %p)\n", programGroup, sbtRecordHeaderHostPointer);
+    return optixFunctionTable_22.optixSbtRecordPackHeader(programGroup, sbtRecordHeaderHostPointer);
+}
+
+static OptixResult __cdecl optixLaunch_22(OptixPipeline pipeline, CUstream stream, CUdeviceptr pipelineParams, size_t pipelineParamsSize, const void *sbt, unsigned int width, unsigned int height, unsigned int depth)
+{
+    TRACE("(%p, %p, %p, %zu, %p, %u, %u, %u)\n", pipeline, stream, pipelineParams, pipelineParamsSize, sbt, width, height, depth);
+    return optixFunctionTable_22.optixLaunch(pipeline, stream, pipelineParams, pipelineParamsSize, sbt, width, height, depth);
+}
+
+static OptixResult __cdecl optixDenoiserCreate_22(OptixDeviceContext context, const void *options, OptixDenoiser *returnHandle)
+{
+    TRACE("(%p, %p, %p)\n", context, options, returnHandle);
+    return optixFunctionTable_22.optixDenoiserCreate(context, options, returnHandle);
+}
+
+static OptixResult __cdecl optixDenoiserDestroy_22(OptixDenoiser handle)
+{
+    TRACE("(%p)\n", handle);
+    return optixFunctionTable_22.optixDenoiserDestroy(handle);
+}
+
+static OptixResult __cdecl optixDenoiserComputeMemoryResources_22(const OptixDenoiser handle, unsigned int maximumInputWidth, unsigned int maximumInputHeight, void *returnSizes)
+{
+    TRACE("(%p, %u, %u, %p)\n", handle, maximumInputWidth, maximumInputHeight, returnSizes);
+    return optixFunctionTable_22.optixDenoiserComputeMemoryResources(handle, maximumInputWidth, maximumInputHeight, returnSizes);
+}
+
+static OptixResult __cdecl optixDenoiserSetup_22(OptixDenoiser denoiser, CUstream stream, unsigned int inputWidth, unsigned int inputHeight, CUdeviceptr state, size_t stateSizeInBytes, CUdeviceptr scratch, size_t scratchSizeInBytes)
+{
+    TRACE("(%p, %p, %u, %u, %p, %zu, %p, %zu)\n", denoiser, stream, inputWidth, inputHeight, state, stateSizeInBytes, scratch, scratchSizeInBytes);
+    return optixFunctionTable_22.optixDenoiserSetup(denoiser, stream, inputWidth, inputHeight, state, stateSizeInBytes, scratch, scratchSizeInBytes);
+}
+
+static OptixResult __cdecl optixDenoiserInvoke_22(OptixDenoiser denoiser, CUstream stream, const void *params, CUdeviceptr denoiserState, size_t denoiserStateSizeInBytes, const void *layers, unsigned int numLayers, unsigned int inputOffsetX, unsigned int inputOffsetY, const void *outputLayer, CUdeviceptr scratch, size_t scratchSizeInBytes)
+{
+    TRACE("(%p, %p, %p, %p, %zu, %p, %u, %u, %u, %p, %p, %zu)\n", denoiser, stream, params, denoiserState, denoiserStateSizeInBytes, layers, numLayers, inputOffsetX, inputOffsetY, outputLayer, scratch, scratchSizeInBytes);
+    return optixFunctionTable_22.optixDenoiserInvoke(denoiser, stream, params, denoiserState, denoiserStateSizeInBytes, layers, numLayers, inputOffsetX, inputOffsetY, outputLayer, scratch, scratchSizeInBytes);
+}
+
+static OptixResult __cdecl optixDenoiserSetModel_22(OptixDenoiser handle, int kind, void* data, size_t sizeInBytes)
+{
+    TRACE("(%p, %d, %p, %zu)\n", handle, kind, data, sizeInBytes);
+    return optixFunctionTable_22.optixDenoiserSetModel(handle, kind, data, sizeInBytes);
+}
+
+static OptixResult __cdecl optixDenoiserComputeIntensity_22(OptixDenoiser handle, CUstream stream, const void *inputImage, CUdeviceptr outputIntensity, CUdeviceptr scratch, size_t scratchSizeInBytes)
+{
+    TRACE("(%p, %p, %p, %p, %p, %zu)\n", handle, stream, inputImage, outputIntensity, scratch, scratchSizeInBytes);
+    return optixFunctionTable_22.optixDenoiserComputeIntensity(handle, stream, inputImage, outputIntensity, scratch, scratchSizeInBytes);
+}
+
 
 /* OptiX ABI = 55 / SDK 7.4.0 */
 
@@ -1388,6 +1642,67 @@ static OptixResult __cdecl optixQueryFunctionTable_36(
     return OPTIX_SUCCESS;
 }
 
+/* OptiX ABI = 22 / SDK 7.0.0 */
+
+static OptixResult __cdecl optixQueryFunctionTable_22(
+    unsigned int numOptions,
+    int* optionKeys,
+    const void** optionValues,
+    void* functionTable,
+    size_t sizeOfTable)
+{
+    if (sizeOfTable != sizeof(OptixFunctionTable_22)) return OPTIX_ERROR_FUNCTION_TABLE_SIZE_MISMATCH;
+
+    OptixResult result = poptixQueryFunctionTable(22, numOptions, optionKeys, optionValues, &optixFunctionTable_22, sizeOfTable);
+
+    if (result != OPTIX_SUCCESS) return result;
+
+    OptixFunctionTable_22 *table = functionTable;
+
+#define ASSIGN_FUNCPTR(f) *(void**)(&table->f) = (void*)&f ## _22
+
+    ASSIGN_FUNCPTR(optixGetErrorName);
+    ASSIGN_FUNCPTR(optixGetErrorString);
+    ASSIGN_FUNCPTR(optixDeviceContextCreate);
+    ASSIGN_FUNCPTR(optixDeviceContextDestroy);
+    ASSIGN_FUNCPTR(optixDeviceContextGetProperty);
+    ASSIGN_FUNCPTR(optixDeviceContextSetLogCallback);
+    ASSIGN_FUNCPTR(optixDeviceContextSetCacheEnabled);
+    ASSIGN_FUNCPTR(optixDeviceContextSetCacheLocation);
+    ASSIGN_FUNCPTR(optixDeviceContextSetCacheDatabaseSizes);
+    ASSIGN_FUNCPTR(optixDeviceContextGetCacheEnabled);
+    ASSIGN_FUNCPTR(optixDeviceContextGetCacheLocation);
+    ASSIGN_FUNCPTR(optixDeviceContextGetCacheDatabaseSizes);
+    ASSIGN_FUNCPTR(optixModuleCreateFromPTX);
+    ASSIGN_FUNCPTR(optixModuleDestroy);
+    ASSIGN_FUNCPTR(optixProgramGroupCreate);
+    ASSIGN_FUNCPTR(optixProgramGroupDestroy);
+    ASSIGN_FUNCPTR(optixProgramGroupGetStackSize);
+    ASSIGN_FUNCPTR(optixPipelineCreate);
+    ASSIGN_FUNCPTR(optixPipelineDestroy);
+    ASSIGN_FUNCPTR(optixPipelineSetStackSize);
+    ASSIGN_FUNCPTR(optixAccelComputeMemoryUsage);
+    ASSIGN_FUNCPTR(optixAccelBuild);
+    ASSIGN_FUNCPTR(optixAccelGetRelocationInfo);
+    ASSIGN_FUNCPTR(optixAccelCheckRelocationCompatibility);
+    ASSIGN_FUNCPTR(optixAccelRelocate);
+    ASSIGN_FUNCPTR(optixAccelCompact);
+    ASSIGN_FUNCPTR(optixConvertPointerToTraversableHandle);
+    ASSIGN_FUNCPTR(optixSbtRecordPackHeader);
+    ASSIGN_FUNCPTR(optixLaunch);
+    ASSIGN_FUNCPTR(optixDenoiserCreate);
+    ASSIGN_FUNCPTR(optixDenoiserDestroy);
+    ASSIGN_FUNCPTR(optixDenoiserComputeMemoryResources);
+    ASSIGN_FUNCPTR(optixDenoiserSetup);
+    ASSIGN_FUNCPTR(optixDenoiserInvoke);
+    ASSIGN_FUNCPTR(optixDenoiserSetModel);
+    ASSIGN_FUNCPTR(optixDenoiserComputeIntensity);
+
+#undef ASSIGN_FUNCPTR
+
+    return OPTIX_SUCCESS;
+}
+
 OptixResult __cdecl optixQueryFunctionTable(
     int abiId,
     unsigned int numOptions,
@@ -1433,6 +1748,8 @@ OptixResult __cdecl optixQueryFunctionTable(
             return optixQueryFunctionTable_41(numOptions, optionKeys, optionValues, functionTable, sizeOfTable);
         case 36:
             return optixQueryFunctionTable_36(numOptions, optionKeys, optionValues, functionTable, sizeOfTable);
+        case 22:
+            return optixQueryFunctionTable_22(numOptions, optionKeys, optionValues, functionTable, sizeOfTable);
         default:
             ERR("abiId = %d not supported\n", abiId);
             return OPTIX_ERROR_UNSUPPORTED_ABI_VERSION;
